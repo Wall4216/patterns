fe9fd3384a54e34e90a55ad1266e3114fcc1252d
Wall <ilnazamirhanov61@gmail.com>
2023-01-16 18:02:17 +0300

    add strategy pattern

---
+++ behavioral/object_null.php
@@ -0,0 +1,41 @@
+<?php
+interface Worker
+{
+    public function work();
+}
+class ObjectManager
+{
+    private Worker $worker;
+
+    /**
+     * @param Worker $worker
+     */
+    public function __construct(Worker $worker)
+    {
+        $this->worker = $worker;
+    }
+
+    public function goWork()
+    {
+        $this->worker->work();
+    }
+}
+class Developer implements Worker
+{
+    public function work()
+    {
+        printf('Developer');
+    }
+}
+class NullWorker implements Worker
+{
+    public function work()
+    {
+
+    }
+}
+$developer = new Developer();
+$nulldeveloper = new NullWorker();
+
+$objectManager = new ObjectManager($nulldeveloper);
+$objectManager->goWork();
